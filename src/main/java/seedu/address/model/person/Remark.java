50e670b3f18b76aa4821d7bd4aff05e92e43006a
BILL <xie.yaoren@u.nus.edu>
2020-09-29 23:17:07 +0800

    Update the target User Profile

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,9 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for Remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
