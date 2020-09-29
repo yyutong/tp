977ef450f44ae104e8902669fc6a87e557bdd58c
BILL <xie.yaoren@u.nus.edu>
2020-09-29 22:12:58 +0800

    Tutorial Add command

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,10 +9,6 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
-    /**
-     * Constructor for Remark.
-     */
-
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
