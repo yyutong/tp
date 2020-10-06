e87472c52eca534e2a22eea5e50237014f831cb7
WayneTanKinLun <waynetan2000@gmail.com>
2020-10-06 21:59:48 +0800

    Add category test

---
+++ src/test/java/seedu/address/model/person/CategoryTest.java
@@ -0,0 +1,44 @@
+package seedu.address.model.person;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Category(null));
+    }
+
+    @Test
+    public void constructor_invalidCategory_throwsIllegalArgumentException() {
+        String invalidCategory = "";
+        assertThrows(IllegalArgumentException.class, () -> new Category(invalidCategory));
+    }
+
+    @Test
+    public void isValidCategory() {
+        // null name
+        assertThrows(NullPointerException.class, () -> Category.isValidCategory(null));
+
+        // invalid name
+        assertFalse(Category.isValidCategory("")); // empty string
+        assertFalse(Category.isValidCategory(" ")); // spaces only
+        assertFalse(Category.isValidCategory("^")); // only non-alphanumeric characters
+        assertFalse(Category.isValidCategory("peter*")); // contains non-alphanumeric characters
+
+        // valid name
+        assertTrue(Category.isValidCategory("peter jack")); // alphabets only
+        assertTrue(Category.isValidCategory("12345")); // numbers only
+        assertTrue(Category.isValidCategory("peter the 2nd")); // alphanumeric characters
+        assertTrue(Category.isValidCategory("Capital Tan")); // with capital letters
+        assertTrue(Category.isValidCategory("David Roger Jackson Ray Jr 2nd")); // long names
+        assertTrue(Category.isValidCategory("FoodBeverage"));
+        assertTrue(Category.isValidCategory("FOODBEVERAGE")); // all capital letters
+        assertTrue(Category.isValidCategory("entertainment"));
+        assertTrue(Category.isValidCategory("ENTERTAINMENT"));
+    }
+}
