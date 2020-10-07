1417d87034ce461a484c4aed00306823048b18fd
yuanxing-y <zyx19991208@gmail.com>
2020-10-07 12:09:13 +0800

    Delete test content for Amount to fix error

--- src/test/java/seedu/address/model/person/AmountTest.java
+++ src/test/java/seedu/address/model/person/AmountTest.java
@@ -1,14 +1,5 @@
 package seedu.address.model.person;
 
-import static seedu.address.testutil.Assert.assertThrows;
-
-import org.junit.jupiter.api.Test;
-
 public class AmountTest {
-
-    @Test
-    public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Amount(null));
-    }
-
+    
 }
