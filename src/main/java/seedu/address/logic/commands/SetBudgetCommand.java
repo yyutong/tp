99fd9464254a6afc171ee6ef73087e3a79868316
yuanxing-y <zyx19991208@gmail.com>
2020-10-14 11:42:37 +0800

    Fix nits

--- src/main/java/seedu/address/logic/commands/SetBudgetCommand.java
+++ src/main/java/seedu/address/logic/commands/SetBudgetCommand.java
@@ -1,13 +1,12 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 public class SetBudgetCommand extends Command {
     public static final String COMMAND_WORD = "setBudget";
-    public static final String MESSAGE_SET_BUDGET_SUCCESS = "Successful! Te budget is now S$ %.2f.\n";
+    public static final String MESSAGE_SET_BUDGET_SUCCESS = "Successful! The budget is now S$ %.2f.\n";
     public static final String MESSAGE_SET_BUDGET_FAIL = "Set Budget failed, please enter a valid budget.\n";
 
     private final double budget;
@@ -18,7 +17,7 @@ public class SetBudgetCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
+    public CommandResult execute(Model model) {
         model.setExpenseBookBudget(budget);
         String msg = String.format(MESSAGE_SET_BUDGET_SUCCESS, budget);
         return new CommandResult(msg);
--- src/main/java/seedu/address/logic/commands/ShowBudgetCommand.java
+++ src/main/java/seedu/address/logic/commands/ShowBudgetCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class ShowBudgetCommand extends Command {
@@ -8,17 +7,18 @@ public class ShowBudgetCommand extends Command {
     public static final String COMMAND_WORD = "showBudget";
     public static final String MESSAGE_BUDGET = "Current budget is: S$ %.2f\n";
     public static final String MESSAGE_REMAINING = "Remaining budget is: S$ %.2f\n";
-    public static final String MESSAGE_SETNEW = "Your current budget is S$0.00, \n"
+    public static final String MESSAGE_SETNEW = "Your current budget is S$ %.2f, \n"
             + "please set a new budget with command: setBudget AMOUNT\n";
 
     @Override
-    public CommandResult execute(Model model) throws CommandException {
-        if (model.getExpenseBookBudget() == 0) {
+    public CommandResult execute(Model model) {
+        double budget = model.getExpenseBookBudget();
+        if (model.getExpenseBookBudget() <= 0) {
+            String setNew = String.format(MESSAGE_SETNEW, budget);
             return new CommandResult(MESSAGE_SETNEW);
         } else {
-            String budgetMsg = String.format(MESSAGE_BUDGET, model.getExpenseBookBudget());
-            String remainingMsg = String.format(MESSAGE_REMAINING, model.getExpenseBookRemaining());
-
+            String budgetMsg = String.format(MESSAGE_BUDGET, budget);
+            String remainingMsg = String.format(MESSAGE_REMAINING, budget);
             return new CommandResult(budgetMsg + remainingMsg);
         }
     }
