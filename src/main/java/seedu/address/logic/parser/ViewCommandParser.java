4c3e752ce4999a0b7450bf59c489e8ecc5be09fa
yyutong <1037662939@qq.com>
2020-10-14 09:18:22 +0800

    Modify ViewCommandParser

--- src/main/java/seedu/address/logic/parser/ViewCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewCommandParser.java
@@ -20,9 +20,6 @@ public class ViewCommandParser {
      * @throws ParseException if the user input does not conform the expected format
      */
     public ViewCommand parse(String args) throws ParseException {
-//        String trimmedArgs = args.trim();
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_CATEGORY);
         requireNonNull(args);
         try{
             Index index = ParserUtil.parseIndex(args);
