f6c23e16c35eb5a110c611129073702dc666eb56
yyutong <1037662939@qq.com>
2020-10-14 13:54:55 +0800

    Add ViewCategory command

--- src/main/java/seedu/address/logic/commands/ViewCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewCommand.java
@@ -37,9 +37,9 @@ public class ViewCommand extends Command {
         }
 
         Expense expenseToView = lastShownList.get(targetIndex.getZeroBased());
-        String message = targetIndex.toString() + "\n" + expenseToView.toString();
-//        model.viewExpense(targetIndex);
-        return new CommandResult(String.format(MESSAGE_VIEW_EXPENSE_SUCCESS, message));
+        String prefix = String.format(MESSAGE_VIEW_EXPENSE_SUCCESS, targetIndex.getOneBased());
+        String message = prefix + "\n" + expenseToView.toString();
+        return new CommandResult(message);
     }
 
     @Override
