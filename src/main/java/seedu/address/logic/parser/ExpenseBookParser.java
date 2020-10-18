87504447a26202ff9ca89980f7fd4171193ab5d7
BILL <xie.yaoren@u.nus.edu>
2020-10-18 21:26:09 +0800

    Modify checkStyle

--- src/main/java/seedu/address/logic/parser/ExpenseBookParser.java
+++ src/main/java/seedu/address/logic/parser/ExpenseBookParser.java
@@ -5,7 +5,6 @@ import seedu.address.logic.parser.exceptions.ParseException;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import seedu.address.logic.commands.AddExpenseCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteDescriptionCommand;
