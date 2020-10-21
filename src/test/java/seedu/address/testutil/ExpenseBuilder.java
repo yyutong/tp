d9ff95b263dfcf3cc8b37565278c41cf29dd76b5
BILL <xie.yaoren@u.nus.edu>
2020-10-22 01:51:55 +0800

    Add the AddExpenseTest and Assertion

--- src/test/java/seedu/address/testutil/ExpenseBuilder.java
+++ src/test/java/seedu/address/testutil/ExpenseBuilder.java
@@ -12,7 +12,7 @@ import seedu.address.model.person.Expense;
 public class ExpenseBuilder {
 
     public static final Double DEFAULT_AMOUNT = 10.0;
-    public static final String DEFAULT_DATE = "2020-10-07";
+    public static final String DEFAULT_DATE = "0";
     public static final String DEFAULT_CATEGORY = "FOOD";
     public static final String DEFAULT_DESCRIPTION = "123, Jurong West Ave 6, #08-111";
     private Amount amount;
@@ -52,6 +52,7 @@ public class ExpenseBuilder {
      * Sets the {@code Address} of the {@code Person} that we are building.
      */
     public ExpenseBuilder withDate(String date) {
+        this.date = new Date(date);
         return this;
     }
 
