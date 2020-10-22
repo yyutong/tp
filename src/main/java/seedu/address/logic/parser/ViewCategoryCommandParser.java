6375898a3fef44bc5891fcb3fab86a760b3804d1
yuanxing-y <zyx19991208@gmail.com>
2020-10-22 18:17:54 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/ViewCategoryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ViewCategoryCommandParser.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import seedu.address.logic.commands.ViewCategoryCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
-public class ViewCategoryCommandParser implements Parser<ViewCategoryCommand>{
+public class ViewCategoryCommandParser implements Parser<ViewCategoryCommand> {
     @Override
     public ViewCategoryCommand parse(String args) throws ParseException {
         return new ViewCategoryCommand();
