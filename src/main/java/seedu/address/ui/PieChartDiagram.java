6c092a596080432819500ee1327aa1fd19c07eb0
yyutong <1037662939@qq.com>
2020-10-22 09:56:44 +0800

    Fix ui package, update UG

---
+++ src/main/java/seedu/address/ui/PieChartDiagram.java
@@ -0,0 +1,13 @@
+package seedu.address.ui;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.chart.PieChart;
+import javafx.scene.Group;
+
+public class PieChartDiagram {
+
+}
