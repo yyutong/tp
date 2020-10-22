78863baac54d0e56225ca8dc1329d3502a932c25
yyutong <1037662939@qq.com>
2020-10-22 09:59:42 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/PieChartDiagram.java
+++ src/main/java/seedu/address/ui/PieChartDiagram.java
@@ -1,13 +1,5 @@
 package seedu.address.ui;
 
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.chart.PieChart;
-import javafx.scene.Group;
-
 public class PieChartDiagram {
 
 }
