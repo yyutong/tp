573a715d018986af8c9aad2f66cfa2f1e57b087e
yyutong <1037662939@qq.com>
2020-10-22 20:26:51 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -13,9 +13,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ExpenseBook;
 import seedu.address.model.Model;
-import seedu.address.model.person.Expense;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Person;
+import seedu.address.model.expense.Expense;
 import seedu.address.testutil.ExpenseBookBuilder;
 import seedu.address.testutil.ExpenseBuilder;
 
@@ -84,17 +82,5 @@ public class CommandTestUtil {
         assertEquals(expectedExpenseBook, actualModel.getExpenseBook());
         assertEquals(expectedFilteredList, actualModel.getFilteredExpenseList());
     }
-    /**
-     * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
-     * {@code model}'s address book.
-     */
-    public static void showPersonAtIndex(Model model, Index targetIndex) {
-        assertTrue(targetIndex.getZeroBased() < model.getFilteredPersonList().size());
 
-        Person person = model.getFilteredPersonList().get(targetIndex.getZeroBased());
-        final String[] splitName = person.getName().fullName.split("\\s+");
-        model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
-
-        assertEquals(1, model.getFilteredPersonList().size());
-    }
 }
