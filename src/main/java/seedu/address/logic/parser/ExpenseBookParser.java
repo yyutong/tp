05d7d75a0801d05fea07a512dbc34a5691ac04db
yyutong <1037662939@qq.com>
2020-10-22 20:11:25 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/parser/ExpenseBookParser.java
+++ src/main/java/seedu/address/logic/parser/ExpenseBookParser.java
@@ -6,19 +6,27 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddExpenseCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteDescriptionCommand;
+import seedu.address.logic.commands.DeleteExpenseCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListExpenseByCategoryCommand;
+import seedu.address.logic.commands.ListExpenseCommand;
+import seedu.address.logic.commands.SetBudgetCommand;
+import seedu.address.logic.commands.ShowBudgetCommand;
+import seedu.address.logic.commands.ViewCategoryCommand;
+import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
  * Parses user input.
  */
 public class ExpenseBookParser {
-
     /**
      * Used for initial separation of command word and args.
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
-
     /**
      * Parses user input into command for execution.
      *
@@ -34,36 +42,24 @@ public class ExpenseBookParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
         switch (commandWord) {
-            case ViewCommand.COMMAND_WORD:
-                return new ViewCommandParser().parse(arguments);
-
             case AddExpenseCommand.COMMAND_WORD:
                 return new AddExpenseCommandParser().parse(arguments);
-
             case DeleteExpenseCommand.COMMAND_WORD:
                 return new DeleteExpenseCommandParser().parse(arguments);
-
-            case HelpCommand.COMMAND_WORD:
-                return new HelpCommandParser().parse();
-
             case ListExpenseCommand.COMMAND_WORD:
                 return new ListExpenseCommand();
-
+            case ViewCommand.COMMAND_WORD:
+                return new ViewCommandParser().parse(arguments);
             case ViewCategoryCommand.COMMAND_WORD:
                 return new ViewCategoryCommandParser().parse(arguments);
-
             case DeleteDescriptionCommand.COMMAND_WORD:
                 return new DeleteDescriptionCommandParser().parse(arguments);
-
             case ListExpenseByCategoryCommand.COMMAND_WORD:
                 return new ListExpenseByCategoryCommandParser().parse(arguments);
-
             case ShowBudgetCommand.COMMAND_WORD:
                 return new ShowBudgetCommandParser().parse(arguments);
-
             case SetBudgetCommand.COMMAND_WORD:
                 return new SetBudgetCommandParser().parse(arguments);
-
             default:
                 throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
