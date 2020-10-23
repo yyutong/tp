c953deb160e1dd724c4f1a4713a4d465725cbee3
yuming7144 <yuming7144@gmail.com>
2020-10-23 23:09:59 +0800

    Fix checkstyle error

--- src/test/java/seedu/address/logic/parser/ExpenseBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/ExpenseBookParserTest.java
@@ -6,7 +6,7 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EXPENSE;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddDescriptionCommand;
+import seedu.address.logic.commands.DescriptionCommand;
 import seedu.address.model.expense.Description;
 public class ExpenseBookParserTest {
 
@@ -15,9 +15,9 @@ public class ExpenseBookParserTest {
     @Test
     public void parseCommand_description() throws Exception {
         final Description remark = new Description("Some remark.");
-        AddDescriptionCommand command = (AddDescriptionCommand) parser.parseCommand(
-                AddDescriptionCommand.COMMAND_WORD + " "
+        DescriptionCommand command = (DescriptionCommand) parser.parseCommand(
+                DescriptionCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_EXPENSE.getOneBased() + " " + PREFIX_DESCRIPTION + remark.value);
-        assertEquals(new AddDescriptionCommand(INDEX_FIRST_EXPENSE, remark), command);
+        assertEquals(new DescriptionCommand(INDEX_FIRST_EXPENSE, remark), command);
     }
 }
