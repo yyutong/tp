55ddd32f66372abe24fbe7da635ba2327262c688
WayneTanKinLun <waynetan2000@gmail.com>
2020-10-25 21:56:55 +0800

    Add empty ListExpenseByDateCommand and its parser

---
+++ src/main/java/seedu/address/logic/commands/ListExpenseByDateCommand.java
@@ -0,0 +1,42 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.expense.CategoryContainsKeywordsPredicate;
+
+/**
+ * Finds and lists all expenses in expense book whose category contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class ListExpenseByDateCommand extends Command {
+
+    public static final String COMMAND_WORD = "listbydate";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all expenses whose category contains any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " entertainment";
+
+    private final CategoryContainsKeywordsPredicate predicate;
+
+    public ListExpenseByDateCommand(CategoryContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredExpenseList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_EXPENSES_LISTED_OVERVIEW, model.getFilteredExpenseList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ListExpenseByDateCommand // instanceof handles nulls
+                && predicate.equals(((ListExpenseByDateCommand) other).predicate)); // state check
+    }
+}
