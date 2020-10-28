4579b2a92b09b47e138994844e298a0a298f0231
yuming7144 <yuming7144@gmail.com>
2020-10-29 00:20:28 +0800

    Add more test cases

--- src/test/java/seedu/address/model/ExpenseBookTest.java
+++ src/test/java/seedu/address/model/ExpenseBookTest.java
@@ -1,25 +1,22 @@
 package seedu.address.model;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalExpenses.CLOTHES;
 import static seedu.address.testutil.TypicalExpenses.getTypicalExpenseBook;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.expense.Budget;
+import seedu.address.model.expense.Currency;
 import seedu.address.model.expense.Expense;
-import seedu.address.testutil.ExpenseBuilder;
 
 class ExpenseBookTest {
     private final ExpenseBook expenseBook = new ExpenseBook();
@@ -40,60 +37,51 @@ class ExpenseBookTest {
         expenseBook.resetData(newData);
         assertEquals(newData, expenseBook);
     }
+    @Test
+    public void hasExpense_nullExpense_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> expenseBook.hasExpense(null));
+    }
 
-//    @Test
-//    public void resetData_withDuplicatePersons_throwsDuplicatePersonException() {
-//        // Two persons with the same identity fields
-//        Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
-//                .build();
-//        List<Person> newPersons = Arrays.asList(ALICE, editedAlice);
-//        AddressBookStub newData = new AddressBookStub(newPersons);
-//
-//        assertThrows(DuplicatePersonException.class, () -> addressBook.resetData(newData));
-//    }
-//
-//    @Test
-//    public void hasPerson_nullPerson_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> addressBook.hasPerson(null));
-//    }
-//
-//    @Test
-//    public void hasPerson_personNotInAddressBook_returnsFalse() {
-//        assertFalse(addressBook.hasPerson(ALICE));
-//    }
-//
-//    @Test
-//    public void hasPerson_personInAddressBook_returnsTrue() {
-//        addressBook.addPerson(ALICE);
-//        assertTrue(addressBook.hasPerson(ALICE));
-//    }
-//
-//    @Test
-//    public void hasPerson_personWithSameIdentityFieldsInAddressBook_returnsTrue() {
-//        addressBook.addPerson(ALICE);
-//        Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
-//                .build();
-//        assertTrue(addressBook.hasPerson(editedAlice));
-//    }
-//
-//    @Test
-//    public void getPersonList_modifyList_throwsUnsupportedOperationException() {
-//        assertThrows(UnsupportedOperationException.class, () -> addressBook.getPersonList().remove(0));
-//    }
-//
-//    /**
-//     * A stub ReadOnlyAddressBook whose persons list can violate interface constraints.
-//     */
-//    private static class AddressBookStub implements ReadOnlyAddressBook {
-//        private final ObservableList<Person> persons = FXCollections.observableArrayList();
-//
-//        AddressBookStub(Collection<Person> persons) {
-//            this.persons.setAll(persons);
-//        }
-//
-//        @Override
-//        public ObservableList<Person> getPersonList() {
-//            return persons;
-//        }
-//    }
+    @Test
+    public void hasExpense_expenseNotInExpenseBook_returnsFalse() {
+        assertFalse(expenseBook.hasExpense(CLOTHES));
+    }
+
+    @Test
+    public void hasExpense_expenseInExpenseBook_returnsFalse() {
+        expenseBook.addExpense(CLOTHES);
+        assertTrue(expenseBook.hasExpense(CLOTHES));
+    }
+
+    @Test
+    public void getExpenseList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> expenseBook.getExpenseList().remove(0));
+    }
+
+    /**
+     * A stub ReadOnlyExpenseBook whose expenses list can violate interface constraints.
+     */
+    private static class ExpenseBookStub implements ReadOnlyExpenseBook {
+        private final ObservableList<Expense> expenses = FXCollections.observableArrayList();
+
+        ExpenseBookStub(Collection<Expense> expenses) {
+            this.expenses.setAll(expenses);
+        }
+
+        @Override
+        public ObservableList<Expense> getExpenseList() {
+            return expenses;
+        }
+        @Override
+        public Currency getCurrency() {
+            return new Currency("SGD");
+        }
+        @Override
+        public void setBudget(Budget budget) {
+        }
+        @Override
+        public Budget getBudget() {
+            return new Budget(1000);
+        }
+    }
 }
---
+++ src/test/java/seedu/address/model/ExpenseModelManagerTest.java
@@ -0,0 +1,125 @@
+package seedu.address.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EXPENSES;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalExpenses.CLOTHES;
+import static seedu.address.testutil.TypicalExpenses.SNACKS;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.GuiSettings;
+import seedu.address.testutil.ExpenseBookBuilder;
+
+class ExpenseModelManagerTest {
+
+    private ExpenseModelManager modelManager = new ExpenseModelManager();
+
+    @Test
+    public void constructor() {
+        assertEquals(new UserPrefs(), modelManager.getUserPrefs());
+        assertEquals(new GuiSettings(), modelManager.getGuiSettings());
+        assertEquals(new ExpenseBook(), new ExpenseBook(modelManager.getExpenseBook()));
+    }
+
+    @Test
+    public void setUserPrefs_nullUserPrefs_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setUserPrefs(null));
+    }
+
+    @Test
+    public void setUserPrefs_validUserPrefs_copiesUserPrefs() {
+        UserPrefs userPrefs = new UserPrefs();
+        userPrefs.setExpenseBookFilePath(Paths.get("expense/book/file/path"));
+        userPrefs.setGuiSettings(new GuiSettings(1, 2, 3, 4));
+        modelManager.setUserPrefs(userPrefs);
+        assertEquals(userPrefs, modelManager.getUserPrefs());
+
+        // Modifying userPrefs should not modify modelManager's userPrefs
+        UserPrefs oldUserPrefs = new UserPrefs(userPrefs);
+        userPrefs.setExpenseBookFilePath(Paths.get("new/expense/book/file/path"));
+        assertEquals(oldUserPrefs, modelManager.getUserPrefs());
+    }
+
+    @Test
+    public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setGuiSettings(null));
+    }
+
+    @Test
+    public void setGuiSettings_validGuiSettings_setsGuiSettings() {
+        GuiSettings guiSettings = new GuiSettings(1, 2, 3, 4);
+        modelManager.setGuiSettings(guiSettings);
+        assertEquals(guiSettings, modelManager.getGuiSettings());
+    }
+
+    @Test
+    public void setExpenseBookFilePath_nullPath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.setExpenseBookFilePath(null));
+    }
+
+    @Test
+    public void setExpenseBookFilePath_validPath_setsExpenseBookFilePath() {
+        Path path = Paths.get("expense/book/file/path");
+        modelManager.setExpenseBookFilePath(path);
+        assertEquals(path, modelManager.getExpenseBookFilePath());
+    }
+
+    @Test
+    public void hasExpense_nullExpense_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.hasExpense(null));
+    }
+
+    @Test
+    public void hasExpense_expenseNotInExpenseBook_returnsFalse() {
+        assertFalse(modelManager.hasExpense(CLOTHES));
+    }
+
+    @Test
+    public void hasPerson_expenseInExpenseBook_returnsTrue() {
+        modelManager.addExpense(CLOTHES);
+        assertTrue(modelManager.hasExpense(CLOTHES));
+    }
+
+    @Test
+    public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredExpenseList().remove(0));
+    }
+
+    @Test
+    public void equals() {
+        ExpenseBook expenseBook = new ExpenseBookBuilder().withExpense(CLOTHES).withExpense(SNACKS).build();
+        ExpenseBook differentExpenseBook = new ExpenseBook();
+        UserPrefs userPrefs = new UserPrefs();
+
+        // same values -> returns true
+        modelManager = new ExpenseModelManager(expenseBook, userPrefs);
+        ExpenseModelManager modelManagerCopy = new ExpenseModelManager(expenseBook, userPrefs);
+        assertTrue(modelManager.equals(modelManagerCopy));
+
+        // same object -> returns true
+        assertTrue(modelManager.equals(modelManager));
+
+        // null -> returns false
+        assertFalse(modelManager.equals(null));
+
+        // different types -> returns false
+        assertFalse(modelManager.equals(5));
+
+        // different expenseBook -> returns false
+        assertFalse(modelManager.equals(new ExpenseModelManager(differentExpenseBook, userPrefs)));
+        // resets modelManager to initial state for upcoming tests
+        modelManager.updateFilteredExpenseList(PREDICATE_SHOW_ALL_EXPENSES);
+
+        // different userPrefs -> returns false
+        UserPrefs differentUserPrefs = new UserPrefs();
+        differentUserPrefs.setExpenseBookFilePath(Paths.get("differentFilePath"));
+        assertFalse(modelManager.equals(new ExpenseModelManager(expenseBook, differentUserPrefs)));
+    }
+
+}
