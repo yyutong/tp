a76494b72bdc0552876ae39f71391446a6eb8230
yuming7144 <yuming7144@gmail.com>
2020-10-29 00:42:22 +0800

    Add more test cases

--- src/test/java/seedu/address/storage/JsonAdaptedExpenseTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedExpenseTest.java
@@ -5,23 +5,18 @@ import static seedu.address.storage.JsonAdaptedExpense.MISSING_FIELD_MESSAGE_FOR
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalExpenses.CLOTHES;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.expense.Amount;
-import seedu.address.model.expense.Date;
 
 public class JsonAdaptedExpenseTest {
-    private static final String INVALID_AMOUNT = "-10.0";
-    private static final String INVALID_CATEGORY = "FOOD-BEVERAGE";
-    private static final String INVALID_DATE_DECIMAL = "1.5";
-    private static final String INVALID_DATE_NEGATIVE = "1.5";
-    private static final String INVALID_DATE = "2020-09-32";
-    private static final String VALID_AMOUNT = String.format("%.2f", CLOTHES.getAmount().value);
+    //private static final String INVALID_AMOUNT = "-10.0";
+    //private static final String INVALID_CATEGORY = "FOOD-BEVERAGE";
+    //private static final String INVALID_DATE_DECIMAL = "1.5";
+    //private static final String INVALID_DATE_NEGATIVE = "1.5";
+    //private static final String INVALID_DATE = "2020-09-32";
+    //private static final String VALID_AMOUNT = String.format("%.2f", CLOTHES.getAmount().value);
     private static final String VALID_CURRENCY = CLOTHES.getCurrency().toString();
     private static final String VALID_CATEGORY = CLOTHES.getCategory().toString();
     private static final String VALID_DATE = CLOTHES.getDate().toString();
