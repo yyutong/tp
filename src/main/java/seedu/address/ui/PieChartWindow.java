0c11d562a5ce8d91ed22e440ef5d3248a6b04ca7
yyutong <1037662939@qq.com>
2020-10-29 13:20:19 +0800

    update ui

--- src/main/java/seedu/address/ui/PieChartWindow.java
+++ src/main/java/seedu/address/ui/PieChartWindow.java
@@ -79,6 +79,7 @@ public class PieChartWindow extends UiPart<Stage> {
             PIECHART.setTitle("Statistics of Expense Book");
             PIECHART.setClockwise(false);
             PIECHART.setLabelsVisible(true);
+
         }
 
     }
