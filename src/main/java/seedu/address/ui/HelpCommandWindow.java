45995c613e660b9b6490cb0652a962287785fb98
BILL <xie.yaoren@u.nus.edu>
2020-10-31 00:40:36 +0800

    Modify the Help Function

--- src/main/java/seedu/address/ui/HelpCommandWindow.java
+++ src/main/java/seedu/address/ui/HelpCommandWindow.java
@@ -1,24 +1,17 @@
 package seedu.address.ui;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.logic.Logic;
 import seedu.address.model.expense.CommandList;
-import seedu.address.model.expense.CurrencyFullName;
-import seedu.address.model.expense.StatisticSummary;
-
-import java.util.List;
-import java.util.logging.Logger;
 
 public class HelpCommandWindow extends UiPart<Stage> {
     private static final String FXML = "HelpCommandWindow.fxml";
