9fb4016e43b93090f427611ae33c2c691140375d
yyutong <1037662939@qq.com>
2020-10-31 16:20:11 +0800

    no message

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HelpCommand.SUCCESSFUL_MESSAGE;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.model.ExpenseModelManager;
