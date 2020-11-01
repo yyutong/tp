47f0a6fb4403091c6bc9e36b4e6718e055b4be04
yyutong <1037662939@qq.com>
2020-11-01 19:27:22 +0800

    checkstyle

--- src/main/java/seedu/address/ui/BudgetPanel.java
+++ src/main/java/seedu/address/ui/BudgetPanel.java
@@ -1,13 +1,13 @@
 package seedu.address.ui;
 
+import java.util.logging.Logger;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.Logic;
 import seedu.address.model.expense.Budget;
-
-import java.util.logging.Logger;
 
 public class BudgetPanel extends UiPart<Region> {
     private static final String FXML = "BudgetPanel.fxml";
