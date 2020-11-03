5d6f1a01cb391617105ef1e25bd0c2e2f836449f
BILL <xie.yaoren@u.nus.edu>
2020-11-03 23:25:25 +0800

    Add in more test

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_BOOKS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_CATEGORY_BOOKS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showExpenseAtIndex;
 import static seedu.address.testutil.TypicalExpenses.getTypicalExpenseBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_EXPENSE;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_EXPENSE;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.EditCommand.EditExpenseDescriptor;
 import seedu.address.model.ExpenseBook;
@@ -67,5 +72,70 @@ public class EditCommandTest {
 
         assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
+
+    @Test
+    public void execute_filteredList_success() {
+        showExpenseAtIndex(model, INDEX_FIRST_EXPENSE);
+
+        Expense personInFilteredList = model.getFilteredExpenseList().get(INDEX_FIRST_EXPENSE.getZeroBased());
+        Expense editedPerson = new ExpenseBuilder(personInFilteredList).withAmount(VALID_AMOUNT_BOOKS).build();
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_EXPENSE,
+                new EditExpenseDescriptorBuilder().withAmount(VALID_AMOUNT_BOOKS).build());
+
+        String expectedMessage = String.format(EditCommand.SUCCESSFUL_MESSAGE, editedPerson);
+
+        Model expectedModel = new ExpenseModelManager(new ExpenseBook(model.getExpenseBook()), new UserPrefs());
+        expectedModel.setExpense(model.getFilteredExpenseList().get(0), editedPerson);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_duplicatePersonUnfilteredList_failure() {
+        Expense firstPerson = model.getFilteredExpenseList().get(INDEX_FIRST_EXPENSE.getZeroBased());
+        EditExpenseDescriptor descriptor = new EditExpenseDescriptorBuilder(firstPerson).build();
+        EditCommand editCommand = new EditCommand(INDEX_SECOND_EXPENSE, descriptor);
+
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_EXPENSE);
+    }
+
+    @Test
+    public void execute_duplicatePersonFilteredList_failure() {
+        showExpenseAtIndex(model, INDEX_FIRST_EXPENSE);
+
+        // edit Expense filtered list into a duplicate in Expense book
+        Expense personInList = model.getExpenseBook().getExpenseList().get(INDEX_SECOND_EXPENSE.getZeroBased());
+        EditCommand editCommand = new EditCommand(INDEX_FIRST_EXPENSE,
+                new EditExpenseDescriptorBuilder(personInList).build());
+
+        assertCommandFailure(editCommand, model, EditCommand.MESSAGE_DUPLICATE_EXPENSE);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredExpenseList().size() + 1);
+        EditExpenseDescriptor descriptor = new EditExpenseDescriptorBuilder().withAmount(VALID_AMOUNT_BOOKS).build();
+        EditCommand editCommand = new EditCommand(outOfBoundIndex, descriptor);
+
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_EXPENSE_DISPLAYED_INDEX);
+    }
+
+    /**
+     * Edit filtered list where index is larger than size of filtered list,
+     * but smaller than size of address book
+     */
+    @Test
+    public void execute_invalidPersonIndexFilteredList_failure() {
+        showExpenseAtIndex(model, INDEX_FIRST_EXPENSE);
+        Index outOfBoundIndex = INDEX_SECOND_EXPENSE;
+        // ensures that outOfBoundIndex is still in bounds of address book list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getExpenseBook().getExpenseList().size());
+
+        EditCommand editCommand = new EditCommand(outOfBoundIndex,
+                new EditExpenseDescriptorBuilder().withAmount(VALID_AMOUNT_BOOKS).build());
+
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_EXPENSE_DISPLAYED_INDEX);
+    }
+
 
 }
