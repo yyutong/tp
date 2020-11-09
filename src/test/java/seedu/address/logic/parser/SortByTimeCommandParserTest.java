b5b01f4bca2ba470b5ceb56067cb66afdebce45b
yyutong <1037662939@qq.com>
2020-11-09 15:04:58 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/parser/SortByTimeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SortByTimeCommandParserTest.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSucces
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.SortByTimeCommand;
+
 import seedu.address.testutil.TypicalOrder;
 
 public class SortByTimeCommandParserTest {
